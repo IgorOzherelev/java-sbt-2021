eff3bd0d7bca3f7222060b6211021662f5fb4fc3
IgorOzherelev <i9163073959@gmail.com>
2021-12-13 23:38:20 +0300

    исправление 2ого дз

--- hw2/src/main/java/mipt/bit/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package mipt.bit.utils;
-
-public final class Utils {
-    public static void checkNotNull(String name, Object obj) {
-        if (obj == null) {
-            throw new IllegalArgumentException("Provided null " + name + " object");
-        }
-    }
-}
